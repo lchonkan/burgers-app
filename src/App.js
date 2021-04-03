d661bdfceeedb69633646afe07ca2675bb1d0bbc
Leonardo Chonkan <leonardo.chonkan@autodesk.com>
2021-04-03 14:19:08 -0600

    added type validation using proptypes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout> Test</Layout>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
