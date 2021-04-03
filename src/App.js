031aeb6bbfe393a0dc6dc078de17888822162887
Leonardo Chonkan <leonardo.chonkan@autodesk.com>
2021-04-03 13:50:35 -0600

    'created the layout component and the aux component' inserted the layout in app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout> Test</Layout>
+      </div>
+    );
   }
 }
 
