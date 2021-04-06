40d526fec451493947c4810fdc8fdcfbb054f3d6
Leonardo Chonkan <lchonkan@forgebim.com>
2021-04-05 22:26:02 -0600

    calculates the sum of the ingredients (count). Displays a paragraph indicating to add ingredients if the sum is zero

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  console.log('Object Keys', Object.keys(props.ingredients));
+  console.log('Object Values', Object.values(props.ingredients));
+  console.log('Object Entries', Object.entries(props.ingredients));
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  console.log('Transformed ingredients:', transformedIngredients);
+
+  // Check for the length of the array
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
