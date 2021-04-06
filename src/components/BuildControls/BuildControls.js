6f7cedfb1c950ed1b4bb81b6032f01c3115e25db
Leonardo Chonkan <leonardo.chonkan@autodesk.com>
2021-04-05 23:03:55 -0600

    added BuildControls to the Burger Builder componen

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.module.css';
 
-const buildControls = (props) => <div></div>;
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
+  </div>
+);
 
 export default buildControls;
