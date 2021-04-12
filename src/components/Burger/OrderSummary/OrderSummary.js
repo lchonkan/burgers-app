ad6d85a65b28c18b3ee20722e2d1fcb4de07ac63
Leonardo Chonkan <leonardo.chonkan@autodesk.com>
2021-04-11 20:14:34 -0600

    created the modal and the order summary compoments

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>
+          {igKey}:{props.ingredients[igKey]}
+        </span>
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>A delicious burger with the following ingredients</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout</p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/BuildControls/BuildControls';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 // global constant
 const INGREDIENT_PRICES = {
@@ -87,6 +89,9 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
